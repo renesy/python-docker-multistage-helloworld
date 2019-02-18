a11c8417914b322cd459abc24ced9aac734b41a4
Robert Sprunk <47247235+renesy@users.noreply.github.com>
2019-02-18 11:31:20 +0100

    Add a crypto example

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,9 +1,17 @@
 import sys
 
+from Crypto.Cipher import AES
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    print("Hello, world")
+    obj = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
+    message = "Hello, world"
+    ciphertext = obj.encrypt(message)
+    print(ciphertext)
+    obj2 = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
+    decipher_message = obj2.decrypt(ciphertext).decode()
+    print(f">{decipher_message}<")
 
     return 0
